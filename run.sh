114a7d92bbba82e70db34c5b7e9e042bb563b0b9
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-02-07 01:44:49 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,3 +0,0 @@
-export OPENAI_API_KEY=sk-a0c9d9q9CHK4qMsYLHuYvgfMglf23aWoeMsJxDl1ULRogGEm
-export OPENAI_BASE_URL=https://api.chatanywhere.tech/v1
-BACKEND=DUCKDUCKGO LLM_TYPE=OPENAI LLM_MODEL=gpt-3.5-turbo python search.py
