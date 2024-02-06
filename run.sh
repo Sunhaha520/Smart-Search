41d5d83dc35e06ad8d3bb75568141c4638f1a064
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-02-07 01:42:35 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,3 @@
+export OPENAI_API_KEY=sk-a0c9d9q9CHK4qMsYLHuYvgfMglf23aWoeMsJxDl1ULRogGEm
+export OPENAI_BASE_URL=https://api.chatanywhere.tech/v1
+BACKEND=DUCKDUCKGO LLM_TYPE=OPENAI LLM_MODEL=gpt-3.5-turbo python search.py
